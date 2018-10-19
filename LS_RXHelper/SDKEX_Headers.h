1ad9e2ce5fa753d1fc9ed56d857d24d77c3c2cf5
IE <leftspace89@users.noreply.github.com>
2018-10-19 03:07:23 +0300

    MAKE_GET & MAKE_RAW SECURITY fix @RMAN

--- LS_RXHelper/SDKEX_Headers.h
+++ LS_RXHelper/SDKEX_Headers.h
@@ -1,22 +1,34 @@
 #pragma once
 
-#define MAKE_GET(NAME,TYPE,FUNCTIONDEF) \
-TYPE Get##NAME (){					\
-TYPE T;						\
-FUNCTIONDEF(Object,&T);						\
-return T;					\
-}
-
-#define MAKE_RAW(NAME,TYPE,FUNCTIONDEF) \
-TYPE NAME (){					\
-TYPE T;						\
-FUNCTIONDEF(Object,&T);						\
-return T;					\
-}
 #include "rVectors.hpp"
 #include "../../sdkapi.h"
+#define LSFail if(!Object){SdkUiConsoleWrite("Object issue, %s",__FUNCTION__); MessageBoxA(0,__FUNCTION__,"FUUUUUK",0);}//assert(Object !=NULL);
+
+
+
 extern PSDK_CONTEXT SDK_CONTEXT_GLOBAL;
 extern  void* g_LocalPlayer; // for every gameobject
+
+#define MAKE_GET(NAME,TYPE,FUNCTIONDEF)                    \
+TYPE Get##NAME##(){                                        \
+    TYPE _##NAME;                                        \
+    SDKSTATUS _STATE = FUNCTIONDEF(Object,&_##NAME);    \
+    if (!SDKSTATUS_SUCCESS(_STATE)) {                    \
+        SdkUiConsoleWrite(" [SDK] Error! MAKE_GET Could not retrieve %s for obj %p errorCode: %i.\n", __FUNCTION__, Object, (int)_STATE); \
+    }                                                    \
+    return _##NAME;                                     \
+}
+
+#define MAKE_RAW(NAME,TYPE,FUNCTIONDEF)                    \
+TYPE NAME##(){                                        \
+    TYPE _##NAME;                                        \
+    SDKSTATUS _STATE = FUNCTIONDEF(Object,&_##NAME);    \
+    if (!SDKSTATUS_SUCCESS(_STATE)) {                    \
+        SdkUiConsoleWrite(" [SDK] MAKE_RAW Error! Could not retrieve %s for obj %p errorCode: %i.\n", __FUNCTION__, Object, (int)_STATE); \
+    }                                                    \
+    return _##NAME;                                     \
+}
+
 #include "GameObject.hpp"
 #include "AttackableUnit.hpp"
 #include "AIBaseClient.hpp"
@@ -25,3 +37,5 @@ extern  void* g_LocalPlayer; // for every gameobject
 #include "AIHeroClient.hpp"
 #include "EntityManager.hpp"
 #include "lpred.h"
+#include "boolinq.h"
+
